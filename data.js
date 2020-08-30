bd3e1d57e44cdf7c4ddfbaced4536895e5cd66f7
Brooks <brooks@brookss-air.gateway.pace.com>
2020-08-30 11:36:20 -0500

    Initial set up

---
+++ data.js
@@ -0,0 +1,162 @@
+const brands = [
+    [{
+
+
+            iconName: "1",
+            brandName: "neuron",
+            color: "#333333",
+            imgSource: "neuron.png"
+        },
+        {
+            iconName: "2",
+            brandName: "nerve_tissue",
+            color: "#a4c639",
+            imgSource: "braintissue.jpg"
+        },
+        {
+            iconName: "3",
+            brandName: "brain",
+            color: "#000000",
+            imgSource: "brain.png"
+        },
+        {
+            iconName: "4",
+            brandName: "nervous_system",
+            color: "#aaaaaa",
+            imgSource: "nervoussystem.png"
+        },
+        {
+            iconName: "5",
+            brandName: "pig",
+            color: "#333333",
+            imgSource: "pig.jpg"
+        }
+    ],
+    [{
+            iconName: "1",
+            brandName: "plantcell",
+            color: "#111111",
+            imgSource: "plantcell.png"
+        },
+        {
+            iconName: "2",
+            brandName: "plantgroundtissue",
+            color: "#d4af37",
+            imgSource: "plantgroundtissue.png"
+        },
+
+        {
+            iconName: "3",
+            brandName: "leaf",
+            color: "#000000",
+            imgSource: "leaf.png"
+        },
+        {
+            iconName: "4",
+            brandName: "shoot_system",
+            color: "#f57d00",
+            imgSource: "shoot-system.png"
+        },
+        {
+            iconName: "5",
+            brandName: "plant",
+            color: "#3b5998",
+            imgSource: "plant.jpeg"
+        }
+    ],
+
+    [{
+            iconName: "1",
+            brandName: "animalcell",
+            color: "#553c7b",
+            imgSource: "animalcell.jpg"
+        },
+
+        {
+            iconName: "2",
+            brandName: "intestinetissue",
+            color: "#ff6c2c",
+            imgSource: "Small_intestine.jpg"
+        },
+        {
+            iconName: "3",
+            brandName: "stomach",
+            color: "#264de4",
+            imgSource: "stomach.jpg"
+        },
+        {
+            iconName: "4",
+            brandName: "digestive_system",
+            color: "#111111",
+            imgSource: "digestivesystem.png"
+        },
+        {
+            iconName: "5",
+            brandName: "prairie_dog",
+            color: "#ba0c2f",
+            imgSource: "Prairie_Dog.jpg"
+        }
+    ],
+    [{
+            iconName: "1",
+            brandName: "plantcell",
+            color: "#008bcf",
+            imgSource: "plantcell.png"
+        },
+        {
+            iconName: "2",
+            brandName: "roottissue",
+            color: "#7289da",
+            imgSource: "roottissue.jpg"
+        },
+        {
+            iconName: "3",
+            brandName: "carrot",
+            color: "#0db7ed",
+            imgSource: "carrot.png"
+        },
+        {
+            iconName: "4",
+            brandName: "root_system",
+            color: "#ea4c89",
+            imgSource: "rootsystem.png"
+        },
+        {
+            iconName: "5",
+            brandName: "plant2",
+            color: "#007ee5",
+            imgSource: "plant2.jpeg"
+        }
+    ],
+    [{
+            iconName: "1",
+            brandName: "bloodcell",
+            color: "#008bcf",
+            imgSource: "bloodcell.png"
+        },
+        {
+            iconName: "2",
+            brandName: "bloodtissue",
+            color: "#7289da",
+            imgSource: "bloodtissue.jpg"
+        },
+        {
+            iconName: "3",
+            brandName: "bloodorgan",
+            color: "#0db7ed",
+            imgSource: "blood_organ.jpg"
+        },
+        {
+            iconName: "4",
+            brandName: "circulatorysystem",
+            color: "#ea4c89",
+            imgSource: "circulatorysystem.png"
+        },
+        {
+            iconName: "5",
+            brandName: "frog",
+            color: "#007ee5",
+            imgSource: "frog.jpg"
+        }
+    ]
+]
---
+++ main.js
@@ -0,0 +1,165 @@
+let correct = 0;
+let total = 0;
+const totalDraggableItems = 5;
+const totalMatchingPairs = 5; // Should be <= totalDraggableItems
+
+const scoreSection = document.querySelector(".score");
+const correctSpan = scoreSection.querySelector(".correct");
+const totalSpan = scoreSection.querySelector(".total");
+const playAgainBtn = scoreSection.querySelector("#play-again-btn");
+
+const draggableItems = document.querySelector(".draggable-items");
+const matchingPairs = document.querySelector(".matching-pairs");
+let draggableElements;
+let droppableElements;
+let cycleCount = 0;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+initiateGame();
+
+
+function initiateGame() {
+    //    const setOfBrands = getRandomInt(brands.length)
+    console.log(cycleCount);
+    const randomDraggableBrands = generateRandomItemsArray(totalDraggableItems, brands[cycleCount]);
+    const randomDroppableBrands = totalMatchingPairs < totalDraggableItems ? generateRandomItemsArray(totalMatchingPairs, randomDraggableBrands) : randomDraggableBrands;
+    const alphabeticallySortedRandomDroppableBrands = [...randomDroppableBrands].sort((a, b) => a.iconName.toLowerCase().localeCompare(b.iconName.toLowerCase()));
+    cycleCount += 1;
+    if (cycleCount > brands.length - 1) {
+        cycleCount = 0;
+    }
+    // Create "draggable-items" and append to DOM
+    for (let i = 0; i < randomDraggableBrands.length; i++) {
+        draggableItems.insertAdjacentHTML("beforeend", ` <img src=images/${randomDraggableBrands[i].imgSource} class= "draggable" draggable="true" id = "${randomDraggableBrands[i].brandName}" width = "90px" height = "90px"> `);
+    }
+
+    //
+    // Create "matching-pairs" and append to DOM
+    for (let i = 0; i < alphabeticallySortedRandomDroppableBrands.length; i++) {
+        matchingPairs.insertAdjacentHTML("beforeend", `
+        <div class="matching-pair">
+          <span class="droppable" data-brand="${alphabeticallySortedRandomDroppableBrands[i].brandName}">${alphabeticallySortedRandomDroppableBrands[i].iconName}</span>
+        </div>
+      `);
+    }
+
+    //Setting the event listeners on all the created DOM elements
+
+    draggableElements = document.querySelectorAll(".draggable");
+    droppableElements = document.querySelectorAll(".droppable");
+
+    draggableElements.forEach(elem => {
+        elem.addEventListener("dragstart", dragStart);
+        // elem.addEventListener("drag", drag);
+        // elem.addEventListener("dragend", dragEnd);
+    });
+
+    droppableElements.forEach(elem => {
+        elem.addEventListener("dragenter", dragEnter);
+        elem.addEventListener("dragover", dragOver);
+        elem.addEventListener("dragleave", dragLeave);
+        elem.addEventListener("drop", drop);
+    });
+}
+
+// Drag and Drop Functions
+
+//Events fired on the drag target
+
+function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id); // or "text/plain"
+
+}
+
+//Events fired on the drop target
+
+function dragEnter(event) {
+    if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+        event.target.classList.add("droppable-hover");
+    }
+}
+
+function dragOver(event) {
+    if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+        event.preventDefault();
+    }
+}
+
+function dragLeave(event) {
+    if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+        event.target.classList.remove("droppable-hover");
+    }
+}
+
+function drop(event) {
+    event.preventDefault();
+    event.target.classList.remove("droppable-hover");
+    const draggableElementBrand = event.dataTransfer.getData("text");
+    const droppableElementBrand = event.target.getAttribute("data-brand");
+
+    const isCorrectMatching = draggableElementBrand === droppableElementBrand;
+    total++;
+    if (isCorrectMatching) {
+        const draggableElement = document.getElementById(draggableElementBrand);
+        event.target.classList.add("dropped");
+        draggableElement.classList.add("dragged");
+        draggableElement.setAttribute("draggable", "false");
+
+        event.target.innerHTML = `<img src=${draggableElement.src}  width = "80px" height = "80px">`;
+        correct++;
+        //<i class="fa fa-${draggableElementBrand}" style="color: ${draggableElement.style.color};"></i>
+        //
+    }
+    scoreSection.style.opacity = 0;
+    setTimeout(() => {
+        correctSpan.textContent = correct;
+        totalSpan.textContent = total;
+        scoreSection.style.opacity = 1;
+    }, 200);
+    if (correct === Math.min(totalMatchingPairs, totalDraggableItems)) { // Game Over!!
+        playAgainBtn.style.display = "block";
+        setTimeout(() => {
+            playAgainBtn.classList.add("play-again-btn-entrance");
+        }, 200);
+    }
+}
+
+// Other Event Listeners
+playAgainBtn.addEventListener("click", playAgainBtnClick);
+
+function playAgainBtnClick() {
+    playAgainBtn.classList.remove("play-again-btn-entrance");
+    correct = 0;
+    total = 0;
+    draggableItems.style.opacity = 0;
+    matchingPairs.style.opacity = 0;
+    setTimeout(() => {
+        scoreSection.style.opacity = 0;
+    }, 100);
+    setTimeout(() => {
+        playAgainBtn.style.display = "none";
+        while (draggableItems.firstChild) draggableItems.removeChild(draggableItems.firstChild);
+        while (matchingPairs.firstChild) matchingPairs.removeChild(matchingPairs.firstChild);
+        initiateGame();
+        correctSpan.textContent = correct;
+        totalSpan.textContent = total;
+        draggableItems.style.opacity = 1;
+        matchingPairs.style.opacity = 1;
+        scoreSection.style.opacity = 1;
+    }, 500);
+}
+
+// Auxiliary functions
+function generateRandomItemsArray(n, originalArray) {
+    let res = [];
+    let clonedArray = [...originalArray];
+    if (n > clonedArray.length) n = clonedArray.length;
+    for (let i = 1; i <= n; i++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length);
+        res.push(clonedArray[randomIndex]);
+        clonedArray.splice(randomIndex, 1);
+    }
+    return res;
+}
